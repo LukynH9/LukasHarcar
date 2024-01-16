4144e87fbb8ab23fd29bae637515151afcb2647d
harca32084 <harluk@seznam.cz>
2024-01-16 11:27:25 +0100

    16.1

--- Lukyn/polls/models.py
+++ Lukyn/polls/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 class  polls (models.Model):
-    name = models.CharField
-    color = models.CharField
-    url = models.URLField
+    title = models.CharField(max_length=200, default="x")
+    name = models.CharField(max_length=200,default="x")
+    play = models.CharField(max_length=200,default="x")
+    play2 = models.URLField(max_length=200,default="x")
+
+class post(models.Model):
+    author = models.CharField(max_length=200, default="x")
+    title = models.CharField(max_length=200, default="x")
+    mood = models.CharField(max_length=200, default="x")
+    amount = models.DecimalField(max_digits=10,decimal_places=2,default="2")
