10ac96790905d8ec8691e928886ed84f244355e4
harca32084 <harluk@seznam.cz>
2024-01-30 10:39:50 +0100

    30.1

--- Lukyn/polls/urls.py
+++ Lukyn/polls/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.models_list)
+    path('randomnuber/',views.models_list)
     ]
--- Lukyn/polls/views.py
+++ Lukyn/polls/views.py
@@ -1,7 +1,10 @@
+import random
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def models_list(reguest):
-    return HttpResponse("Testing of views")
+    random_number = random.randint(a 1, b 100)
+    return HttpResponse(f"Random number"  )
