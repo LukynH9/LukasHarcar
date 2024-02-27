245e1160bfca3182c305aa30721192d44244acc0
harca32084 <harluk@seznam.cz>
2024-02-27 11:28:57 +0100

    27.2

--- Lukyn/polls/views.py
+++ Lukyn/polls/views.py
@@ -5,19 +5,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-class RandomNumberGenerator:
-    def __init__(self):
-        self.counter = 0
-
-    def generate_number(self):
-        self.counter += 1
-        if random.random() < 0.1:  # Pravděpodobnost, že padne 0 je 10%
-            return 0
-        else:
-            return random.randint(1, 10)
-
-generator = RandomNumberGenerator()
-
 def models_list(request):
-    random_number = generator.generate_number()
-    return HttpResponse(f"Random number: {random_number}")
+    from django.http import HttpResponse
+    import random
+
+    class RandomNumberGenerator:
+        def __init__(self):
+            self.counter = 0
+
+        def generate_number(self):
+            self.counter += 1
+            if self.counter % 5 == 0:
+                return 0
+            else:
+                return random.randint(1, 1000)
+
+    generator = RandomNumberGenerator()
+
+    def get_random_number():
+        random_number = generator.generate_number()
+        return random_number
+
+    gamble = random_result = get_random_number()
+    print(f"Random number: {random_result}")
+    return render(request,'polls/polls.html', {"random_number": gamble})
